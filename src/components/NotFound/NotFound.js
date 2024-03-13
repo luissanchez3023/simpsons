71ab40f5b0fca9146249c1310a54fe9ec07fd329
Luis Eduardo Sanchez Reyes <luiseduardo.sanchez@globant.com>
2024-03-13 18:37:59 -0500

    Añadiendo simpsons

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,22 @@
+import './NotFound.css';
+
+function NotFound() {
+    return (
+    <div className='notfound_page'>
+    <header>
+      <p className='errorTitle'>404</p>
+    </header>
+    <section>
+        <main>
+            
+        </main>
+    </section>
+    <footer>
+      <p>Error for load this page</p>
+    </footer>
+  </div>
+  )
+
+}
+
+export default NotFound;
